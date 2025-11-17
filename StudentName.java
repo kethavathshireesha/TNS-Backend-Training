16d3202b3054aa72b3f475261d0b1c555a9e3b6f
kethavathshireesha <kethavathshireesha20@gmail.com>
2025-11-17 21:22:42 +0530

    Update StudentName.java

--- StudentName.java
+++ StudentName.java
@@ -20,14 +20,15 @@ public class StudentName{
        // Accessing instance variable using object
         Student s1=new Student();
         s1.rollNo=100;
-        s1.name="Akram";
+        s1.name="siri";
         s1.branch="CSE";
         s1.Print();
         System.out.println(Student.collegeName);
         Student s2=new Student();
         s2.rollNo=101;
-        s2.name="Fasi";
+        s2.name="nandu";
         s2.branch="EEE";
         s2.Print();
     }
+
 }
