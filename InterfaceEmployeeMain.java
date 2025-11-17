c2a87d2f54e93ab6f8d8d75cc65c5f7c6475e00a
kethavathshireesha <kethavathshireesha20@gmail.com>
2025-11-17 21:26:15 +0530

    Update InterfaceEmployeeMain.java

--- InterfaceEmployeeMain.java
+++ InterfaceEmployeeMain.java
@@ -3,9 +3,10 @@ public class InterfaceEmployeeMain {
     public static void main(String[] args) {
 
         // Creating an object of FullTimeEmployee
-        FullTimeEmployee emp = new FullTimeEmployee("Akram", 101, 30000);
+        FullTimeEmployee emp = new FullTimeEmployee("siri", 200, 40000);
 
         // Calling methods
         emp.getDetails();
     }
+
     }
